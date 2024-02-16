d146171483b204a798d0480751059dfbc77d8dba
uday-donepudi <udaydonipudi@gmail.com>
2024-02-16 18:30:06 +0530

    login

--- app/src/main/java/com/example/p2k24/MainActivity.java
+++ app/src/main/java/com/example/p2k24/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.p2k24;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.example.p2k24.StudentLogin;
+import com.example.p2k24.TeacherLogin;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ViewPager viewPager = findViewById(R.id.viewPager);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        TabAdapter adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TeacherLogin(), "TEACHER");
+        adapter.addFragment(new StudentLogin(), "STUDENT");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    static class TabAdapter extends androidx.fragment.app.FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public TabAdapter(androidx.fragment.app.FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
     }
 }
